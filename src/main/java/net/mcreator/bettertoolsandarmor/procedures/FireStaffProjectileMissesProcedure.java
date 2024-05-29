c3a7beee9c4c26d11502159a9c93df4d697bb518
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-05-29 10:38:36 +0100

    Tweaked Fire Staff cooldowns

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/FireStaffProjectileMissesProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/FireStaffProjectileMissesProcedure.java
@@ -4,13 +4,16 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
+import net.mcreator.bettertoolsandarmor.init.BetterToolsModEnchantments;
 
 public class FireStaffProjectileMissesProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, Entity immediatesourceentity) {
@@ -29,7 +32,8 @@ public class FireStaffProjectileMissesProcedure {
 				_level.explode(null, x, y, z, (float) immediatesourceentity.getPersistentData().getDouble("explosion_power"), Level.ExplosionInteraction.MOB);
 			if (!(entity instanceof Player _plr ? _plr.getAbilities().instabuild : false)) {
 				if (entity instanceof Player _player)
-					_player.getCooldowns().addCooldown(BetterToolsModItems.FIRE_STAFF.get(), (int) (immediatesourceentity.getPersistentData().getDouble("explosion_power") * 10));
+					_player.getCooldowns().addCooldown(BetterToolsModItems.FIRE_STAFF.get(), (int) (immediatesourceentity.getPersistentData().getDouble("explosion_power")
+							* (10 - (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(BetterToolsModEnchantments.SWIFT_CAST.get()))));
 			}
 		}
 	}
