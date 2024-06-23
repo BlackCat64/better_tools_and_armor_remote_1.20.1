d85ad068e8a295d698409b312572f1f90c7eb3e3
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-06-23 11:22:05 +0100

    Added dynamic tooltip for Crystallite Lapis armor

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteLapisArmorTooltipProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteLapisArmorTooltipProcedure.java
@@ -1,5 +1,7 @@
 package net.mcreator.bettertoolsandarmor.procedures;
 
+import top.theillusivec4.curios.api.CuriosApi;
+
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
@@ -8,6 +10,9 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.network.chat.Component;
 import net.minecraft.client.gui.screens.Screen;
 
@@ -22,23 +27,48 @@ public class CrystalliteLapisArmorTooltipProcedure {
 	@OnlyIn(Dist.CLIENT)
 	@SubscribeEvent
 	public static void onItemTooltip(ItemTooltipEvent event) {
-		execute(event, event.getItemStack(), event.getToolTip());
+		execute(event, event.getEntity(), event.getItemStack(), event.getToolTip());
 	}
 
-	public static void execute(ItemStack itemstack, List<Component> tooltip) {
-		execute(null, itemstack, tooltip);
+	public static void execute(Entity entity, ItemStack itemstack, List<Component> tooltip) {
+		execute(null, entity, itemstack, tooltip);
 	}
 
-	private static void execute(@Nullable Event event, ItemStack itemstack, List<Component> tooltip) {
-		if (tooltip == null)
+	private static void execute(@Nullable Event event, Entity entity, ItemStack itemstack, List<Component> tooltip) {
+		if (entity == null || tooltip == null)
 			return;
+		double percent = 0;
 		if (itemstack.getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_LAPIS_HELMET.get() || itemstack.getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_LAPIS_CHESTPLATE.get()
 				|| itemstack.getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_LAPIS_LEGGINGS.get() || itemstack.getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_LAPIS_BOOTS.get()) {
+			if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.HEAD) : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_LAPIS_HELMET.get()) {
+				percent = percent + 20;
+			}
+			if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_LAPIS_CHESTPLATE.get()) {
+				percent = percent + 20;
+			}
+			if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.LEGS) : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_LAPIS_LEGGINGS.get()) {
+				percent = percent + 20;
+			}
+			if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_LAPIS_BOOTS.get()) {
+				percent = percent + 20;
+			}
+			if (entity instanceof LivingEntity lv ? CuriosApi.getCuriosHelper().findEquippedCurio(BetterToolsModItems.MAGIC_RING.get(), lv).isPresent() : false) {
+				percent = percent + 20;
+			}
+			if (percent >= 80) {
+				percent = 100;
+			}
+			if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == itemstack.getItem()
+					|| (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.LEGS) : ItemStack.EMPTY).getItem() == itemstack.getItem()
+					|| (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY).getItem() == itemstack.getItem()
+					|| (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.HEAD) : ItemStack.EMPTY).getItem() == itemstack.getItem()) {
+				tooltip.add(Component.literal(("\u00A72 " + new java.text.DecimalFormat("###").format(percent) + "% XP Levels kept on death")));
+			} else {
+				tooltip.add(Component.literal("\u00A79+20% XP Levels kept on death"));
+			}
 			if (Screen.hasShiftDown()) {
-				tooltip.add(Component.literal("\u00A77For each armor piece worn:"));
-				tooltip.add(Component.literal("\u00A79+20% of XP Levels kept on death"));
-				tooltip.add(Component.literal("\u00A77When full set worn:"));
-				tooltip.add(Component.literal("\u00A79All XP Levels are kept on death"));
+				tooltip.add(Component.literal("\u00A77Full-set bonus:"));
+				tooltip.add(Component.literal("\u00A79All XP Levels kept on death"));
 			} else {
 				tooltip.add(Component.literal("\u00A78Press Shift for details"));
 			}
