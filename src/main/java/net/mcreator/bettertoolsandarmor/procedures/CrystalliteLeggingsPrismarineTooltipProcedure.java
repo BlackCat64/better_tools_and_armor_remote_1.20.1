3c38a8692c659a5eb9d7d68f83944cfac81c578b
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-07-06 12:22:13 +0100

    Added dynamic tooltip for Crystallite-Prismarine leggings

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteLeggingsPrismarineTooltipProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteLeggingsPrismarineTooltipProcedure.java
@@ -8,6 +8,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.network.chat.Component;
 import net.minecraft.client.gui.screens.Screen;
 
@@ -22,22 +23,36 @@ public class CrystalliteLeggingsPrismarineTooltipProcedure {
 	@OnlyIn(Dist.CLIENT)
 	@SubscribeEvent
 	public static void onItemTooltip(ItemTooltipEvent event) {
-		execute(event, event.getItemStack(), event.getToolTip());
+		execute(event, event.getEntity(), event.getItemStack(), event.getToolTip());
 	}
 
-	public static void execute(ItemStack itemstack, List<Component> tooltip) {
-		execute(null, itemstack, tooltip);
+	public static void execute(Entity entity, ItemStack itemstack, List<Component> tooltip) {
+		execute(null, entity, itemstack, tooltip);
 	}
 
-	private static void execute(@Nullable Event event, ItemStack itemstack, List<Component> tooltip) {
-		if (tooltip == null)
+	private static void execute(@Nullable Event event, Entity entity, ItemStack itemstack, List<Component> tooltip) {
+		if (entity == null || tooltip == null)
 			return;
+		boolean rain = false;
+		boolean water = false;
 		if (itemstack.getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_PRISMARINE_LEGGINGS.get()) {
-			if (Screen.hasShiftDown()) {
-				tooltip.add(Component.literal("\u00A77When in rain:"));
-				tooltip.add(Component.literal("\u00A79-50% Gravity"));
-				tooltip.add(Component.literal("\u00A77When in water:"));
+			if (entity.isInWater()) {
+				water = true;
+				rain = true;
 				tooltip.add(Component.literal("\u00A79Zero Gravity"));
+			} else if (entity.isInWaterRainOrBubble()) {
+				rain = true;
+				tooltip.add(Component.literal("\u00A79-50% Gravity"));
+			}
+			if (Screen.hasShiftDown()) {
+				if (!rain) {
+					tooltip.add(Component.literal("\u00A77When in rain:"));
+					tooltip.add(Component.literal("\u00A79-50% Gravity"));
+				}
+				if (!water) {
+					tooltip.add(Component.literal("\u00A77When in water:"));
+					tooltip.add(Component.literal("\u00A79Zero Gravity"));
+				}
 			} else {
 				tooltip.add(Component.literal("\u00A78Press Shift for details"));
 			}
