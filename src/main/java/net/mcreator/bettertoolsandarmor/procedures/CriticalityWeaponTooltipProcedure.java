c70043135ea57e4af9b06905cc7e36c5a88261e7
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-07-06 11:23:48 +0100

    Added dynamic tooltip for Critical Hit Multiplier attribute.

---
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CriticalityWeaponTooltipProcedure.java
@@ -0,0 +1,44 @@
+package net.mcreator.bettertoolsandarmor.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.bettertoolsandarmor.init.BetterToolsModAttributes;
+
+import javax.annotation.Nullable;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber
+public class CriticalityWeaponTooltipProcedure {
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent
+	public static void onItemTooltip(ItemTooltipEvent event) {
+		execute(event, event.getEntity(), event.getItemStack(), event.getToolTip());
+	}
+
+	public static void execute(Entity entity, ItemStack itemstack, List<Component> tooltip) {
+		execute(null, entity, itemstack, tooltip);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity, ItemStack itemstack, List<Component> tooltip) {
+		if (entity == null || tooltip == null)
+			return;
+		if (((LivingEntity) entity).getAttribute(BetterToolsModAttributes.CRITICALHITMULTIPLIER.get()).getValue() > 1.5) {
+			if (itemstack.is(ItemTags.create(new ResourceLocation("forge:tools")))) {
+				tooltip.add(Component.literal(("\u00A72 " + new java.text.DecimalFormat("##.#").format(((LivingEntity) entity).getAttribute(BetterToolsModAttributes.CRITICALHITMULTIPLIER.get()).getValue()) + "x Critical Hit Multiplier")));
+			}
+		}
+	}
+}
