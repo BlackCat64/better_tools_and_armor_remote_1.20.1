46c2a39d92b0888b2eec9e000720ecc9fddacd10
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-07-20 10:40:22 +0100

    Stuck in the Mud advancement is now obtainable

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteSwordEmeraldTrapProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteSwordEmeraldTrapProcedure.java
@@ -14,11 +14,14 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.CommandSource;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
 
 import javax.annotation.Nullable;
 
@@ -70,6 +73,14 @@ public class CrystalliteSwordEmeraldTrapProcedure {
 									_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.rooted_dirt.fall")), SoundSource.BLOCKS, 1, 1);
 								} else {
 									_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.rooted_dirt.fall")), SoundSource.BLOCKS, 1, 1, false);
+								}
+							}
+							if (sourceentity instanceof ServerPlayer _player) {
+								Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("better_tools:crystallite_sword_emerald_adv"));
+								AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+								if (!_ap.isDone()) {
+									for (String criteria : _ap.getRemainingCriteria())
+										_player.getAdvancements().award(_adv, criteria);
 								}
 							}
 						}
