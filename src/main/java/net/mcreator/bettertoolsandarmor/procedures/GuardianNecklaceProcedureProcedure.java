e1826f1c97d95f94f0f15dd2547bb9a806b85bdf
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-07-21 09:28:00 +0100

    Removed SFX from Guardian Necklace damage.

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/GuardianNecklaceProcedureProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/GuardianNecklaceProcedureProcedure.java
@@ -2,7 +2,6 @@ package net.mcreator.bettertoolsandarmor.procedures;
 
 import top.theillusivec4.curios.api.CuriosApi;
 
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
@@ -11,7 +10,6 @@ import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.monster.Drowned;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.Mob;
@@ -19,13 +17,11 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.tags.TagKey;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.core.BlockPos;
 
 import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
 
@@ -58,22 +54,14 @@ public class GuardianNecklaceProcedureProcedure {
 				final Vec3 _center = new Vec3(x, y, z);
 				List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(20 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
 				for (Entity entityiterator : _entfound) {
-					if (entityiterator.getType().is(TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation("better_tools:hostile_mobs"))) || (entityiterator instanceof Mob _mobEnt ? (Entity) _mobEnt.getTarget() : null) == entity) {
+					if (entityiterator.getType().is(TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation("better_tools:hostile_mobs"))) || (entityiterator instanceof Mob _mobEnt ? (Entity) _mobEnt.getTarget() : null) == entity
+							|| entityiterator == (entity instanceof Mob _mobEnt ? (Entity) _mobEnt.getTarget() : null)) {
 						if (entity.getPersistentData().getDouble("guardian_necklace_damage_timer") == 0) {
 							entityiterator.hurt(
 									new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation("better_tools:water_pulse_damage"))), entity),
-									(float) (entityiterator instanceof Drowned || entityiterator instanceof LivingEntity _livEnt7 && _livEnt7.getMobType() == MobType.WATER ? damage * 2 : damage));
+									(float) (entityiterator instanceof Drowned || entityiterator instanceof LivingEntity _livEnt9 && _livEnt9.getMobType() == MobType.WATER ? damage * 2 : damage));
 							if (world instanceof ServerLevel _level)
 								_level.sendParticles(ParticleTypes.NAUTILUS, (entityiterator.getX()), (entityiterator.getY() + 1), (entityiterator.getZ()), 8, 0.3, 1, 0.3, 0.05);
-							if (world instanceof Level _level) {
-								if (!_level.isClientSide()) {
-									_level.playSound(null, BlockPos.containing(entityiterator.getX(), entityiterator.getY(), entityiterator.getZ()), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("ambient.underwater.enter")),
-											SoundSource.AMBIENT, 1, 1);
-								} else {
-									_level.playLocalSound((entityiterator.getX()), (entityiterator.getY()), (entityiterator.getZ()), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("ambient.underwater.enter")), SoundSource.AMBIENT, 1, 1,
-											false);
-								}
-							}
 						}
 					}
 				}
