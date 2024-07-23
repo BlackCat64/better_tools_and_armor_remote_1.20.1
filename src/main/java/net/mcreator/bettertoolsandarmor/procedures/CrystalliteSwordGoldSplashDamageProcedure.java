4f6fce7abc6f6e73fc5e6ef1295724e1a26b5a77
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-07-23 10:19:55 +0100

    Added cooldown for Crystallite Gold Sword spin attack. Damage now increases when enchanted with Sweeping Edge.

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteSwordGoldSplashDamageProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteSwordGoldSplashDamageProcedure.java
@@ -6,6 +6,7 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageTypes;
@@ -24,21 +25,25 @@ public class CrystalliteSwordGoldSplashDamageProcedure {
 		if (entity == null)
 			return;
 		double sword_damage = 0;
-		double x_velocity = 0;
-		double z_velocity = 0;
-		sword_damage = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SHARPNESS, itemstack) != 0
-				? ((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.ATTACK_DAMAGE).getValue() + 0.5 + 0.5 * itemstack.getEnchantmentLevel(Enchantments.SHARPNESS)
-				: ((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.ATTACK_DAMAGE).getValue();
-		if (world instanceof ServerLevel _level)
-			_level.sendParticles((SimpleParticleType) (BetterToolsModParticleTypes.CRYSTALLITE_SPLASH_DAMAGE.get()), x, (y + 0.75), z, 32, 0.5, 0.05, 0.5, 0.005);
-		{
-			final Vec3 _center = new Vec3(x, (y + 1), z);
-			List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(3 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
-			for (Entity entityiterator : _entfound) {
-				if (!(entityiterator == entity)) {
-					entityiterator.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.PLAYER_ATTACK), entity), (float) (sword_damage * (2 / 3)));
+		double multiplier = 0;
+		if (!(entity instanceof Player _plrCldCheck1 && _plrCldCheck1.getCooldowns().isOnCooldown(itemstack.getItem()))) {
+			sword_damage = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SHARPNESS, itemstack) != 0
+					? ((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.ATTACK_DAMAGE).getValue() + 0.5 + 0.5 * itemstack.getEnchantmentLevel(Enchantments.SHARPNESS)
+					: ((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.ATTACK_DAMAGE).getValue();
+			multiplier = Math.min(0.6 + 0.1 * itemstack.getEnchantmentLevel(Enchantments.SWEEPING_EDGE), 100);
+			if (world instanceof ServerLevel _level)
+				_level.sendParticles((SimpleParticleType) (BetterToolsModParticleTypes.CRYSTALLITE_SPLASH_DAMAGE.get()), x, (y + 0.75), z, 32, 0.5, 0.05, 0.5, 0.005);
+			{
+				final Vec3 _center = new Vec3(x, (y + 1), z);
+				List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(3 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+				for (Entity entityiterator : _entfound) {
+					if (!(entityiterator == entity)) {
+						entityiterator.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.PLAYER_ATTACK), entity), (float) (sword_damage * multiplier));
+					}
 				}
 			}
+			if (entity instanceof Player _player)
+				_player.getCooldowns().addCooldown(itemstack.getItem(), 20);
 		}
 	}
 }
--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteSwordGoldTooltipProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteSwordGoldTooltipProcedure.java
@@ -4,13 +4,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
@@ -24,22 +22,21 @@ public class CrystalliteSwordGoldTooltipProcedure {
 	@OnlyIn(Dist.CLIENT)
 	@SubscribeEvent
 	public static void onItemTooltip(ItemTooltipEvent event) {
-		execute(event, event.getEntity(), event.getItemStack(), event.getToolTip());
+		execute(event, event.getItemStack(), event.getToolTip());
 	}
 
-	public static void execute(Entity entity, ItemStack itemstack, List<Component> tooltip) {
-		execute(null, entity, itemstack, tooltip);
+	public static void execute(ItemStack itemstack, List<Component> tooltip) {
+		execute(null, itemstack, tooltip);
 	}
 
-	private static void execute(@Nullable Event event, Entity entity, ItemStack itemstack, List<Component> tooltip) {
-		if (entity == null || tooltip == null)
+	private static void execute(@Nullable Event event, ItemStack itemstack, List<Component> tooltip) {
+		if (tooltip == null)
 			return;
-		if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:increased_reach_tools")))) {
-			if (itemstack.getItem() == (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem()) {
-				tooltip.add(Component.literal(("\u00A72 " + new java.text.DecimalFormat("##.#").format(((LivingEntity) entity).getAttribute(ForgeMod.BLOCK_REACH.get()).getValue()) + " Block Reach")));
-			} else {
-				tooltip.add(Component.literal("\u00A79+3 Block Reach"));
-			}
+		double percent = 0;
+		if (itemstack.is(ItemTags.create(new ResourceLocation("better_tools:spin_attack_weapons")))) {
+			percent = Math.min(60 + 10 * itemstack.getEnchantmentLevel(Enchantments.SWEEPING_EDGE), 100);
+			tooltip.add(Component.literal("\u00A77Spin Attack Damage:"));
+			tooltip.add(Component.literal(("\u00A79 " + new java.text.DecimalFormat("##.#").format(percent) + "% of Weapon Damage")));
 		}
 	}
 }
