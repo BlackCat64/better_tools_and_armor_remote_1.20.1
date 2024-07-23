9ec1e2ed53dd8d207859ee92435887e79dd0d657
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-07-23 11:12:19 +0100

    Crystallite Sky chestplate knockback is always the same, regardless of the distance between the attacker and the wearer.

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteChestplateSkyProcedureProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteChestplateSkyProcedureProcedure.java
@@ -32,10 +32,24 @@ public class CrystalliteChestplateSkyProcedureProcedure {
 		if (entity == null || immediatesourceentity == null)
 			return;
 		double knockback_power = 0;
+		double distance = 0;
+		double x_diff = 0;
+		double z_diff = 0;
+		double x_velocity = 0;
+		double y_velocity = 0;
+		double z_velocity = 0;
 		if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_SKY_CHESTPLATE.get()) {
 			if (immediatesourceentity instanceof LivingEntity) {
-				knockback_power = Math.max(0.75 * (1 - ((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.KNOCKBACK_RESISTANCE).getValue()), 0);
-				immediatesourceentity.setDeltaMovement(new Vec3(((immediatesourceentity.getX() - entity.getX()) * knockback_power), 0.35, ((immediatesourceentity.getZ() - entity.getZ()) * knockback_power)));
+				knockback_power = Math.max(0.75 * (1 - ((LivingEntity) immediatesourceentity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.KNOCKBACK_RESISTANCE).getValue()), 0);
+				x_diff = immediatesourceentity.getX() - entity.getX();
+				z_diff = immediatesourceentity.getZ() - entity.getZ();
+				distance = Math.sqrt(Math.pow(x_diff, 2) + Math.pow(z_diff, 2));
+				x_velocity = (x_diff / distance) * knockback_power;
+				y_velocity = 0.5 * knockback_power;
+				z_velocity = (z_diff / distance) * knockback_power;
+				if (knockback_power > 0) {
+					immediatesourceentity.setDeltaMovement(new Vec3(x_velocity, y_velocity, z_velocity));
+				}
 			}
 		}
 	}
