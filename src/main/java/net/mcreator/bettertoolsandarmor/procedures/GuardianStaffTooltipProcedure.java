a0d3ab65d9467708df9afd00265923c2bd29ae74
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-07-28 09:47:45 +0100

    Magic Staffs now display correctly in the player's hand, in 3rd person view.

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/GuardianStaffTooltipProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/GuardianStaffTooltipProcedure.java
@@ -42,14 +42,14 @@ public class GuardianStaffTooltipProcedure {
 				damage = 6 + itemstack.getEnchantmentLevel(BetterToolsModEnchantments.ENSORCELLATION.get());
 				radius = 4 + 2 * itemstack.getEnchantmentLevel(BetterToolsModEnchantments.ENSORCELLATION.get());
 				if (entity.isInWaterRainOrBubble()) {
-					damage = damage * 2;
+					damage = damage * 1.5;
 					radius = radius * 1.5;
 				}
 				cooldown = 10 - 1.5 * itemstack.getEnchantmentLevel(BetterToolsModEnchantments.SWIFT_CAST.get());
 				tooltip.add(Component.literal("\u00A77Staff Effects:"));
-				tooltip.add(Component.literal(("\u00A72 " + ("" + damage).replace(".0", "") + " Water Pulse Damage")));
-				tooltip.add(Component.literal(("\u00A72 " + ("" + radius).replace(".0", "") + " Block Radius")));
-				tooltip.add(Component.literal(("\u00A7c " + ("" + cooldown).replace(".0", "") + "s Cooldown")));
+				tooltip.add(Component.literal(("\u00A72 " + new java.text.DecimalFormat("##.#").format(damage) + " Water Pulse Damage")));
+				tooltip.add(Component.literal(("\u00A72 " + new java.text.DecimalFormat("##").format(radius) + " Block Radius")));
+				tooltip.add(Component.literal(("\u00A7c " + new java.text.DecimalFormat("##.#").format(cooldown) + "s Cooldown")));
 			} else {
 				tooltip.add(Component.literal("\u00A78Press Shift for details"));
 			}
