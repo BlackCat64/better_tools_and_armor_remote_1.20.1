a683428bb449559f0dfbe99302168f8ea10d7c8c
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-10-24 13:48:52 +0100

    The frozen effect now constantly puts the mob 'in a cobweb' each tick.

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/FrozenEffectParticlesProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/FrozenEffectParticlesProcedure.java
@@ -2,6 +2,7 @@ package net.mcreator.bettertoolsandarmor.procedures;
 
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
@@ -23,6 +24,7 @@ public class FrozenEffectParticlesProcedure {
 		if (world instanceof ServerLevel _level)
 			_level.sendParticles((SimpleParticleType) (BetterToolsModParticleTypes.ICE_PARTICLE.get()), x, (y + entity.getBbHeight()), z, 1, 0.33, 0.5, 0.33, 0.015);
 		entity.setTicksFrozen(135);
+		entity.makeStuckInBlock(Blocks.AIR.defaultBlockState(), new Vec3(0.25, 0.05, 0.25));
 		display = (Entity) world.getEntitiesOfClass(Display.BlockDisplay.class, AABB.ofSize(new Vec3(x, y, z), 3, 3, 3), e -> true).stream().sorted(new Object() {
 			Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
 				return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/FrozenEffectStopJumpingProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/FrozenEffectStopJumpingProcedure.java
@@ -5,8 +5,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.living.LivingEvent;
 
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
@@ -31,15 +29,12 @@ public class FrozenEffectStopJumpingProcedure {
 		if (entity == null)
 			return;
 		double time = 0;
-		if (entity instanceof LivingEntity _livEnt0 && _livEnt0.hasEffect(BetterToolsModMobEffects.FROZEN.get())) {
-			entity.makeStuckInBlock(Blocks.AIR.defaultBlockState(), new Vec3(0.25, 0.05, 0.25));
-			if (entity instanceof Player) {
-				time = entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(BetterToolsModMobEffects.FROZEN.get()) ? _livEnt.getEffect(BetterToolsModMobEffects.FROZEN.get()).getDuration() : 0;
-				if (entity instanceof LivingEntity _entity)
-					_entity.removeEffect(BetterToolsModMobEffects.FROZEN.get());
-				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-					_entity.addEffect(new MobEffectInstance(BetterToolsModMobEffects.FROZEN.get(), (int) (time - 10), 0, true, false));
-			}
+		if (entity instanceof Player) {
+			time = entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(BetterToolsModMobEffects.FROZEN.get()) ? _livEnt.getEffect(BetterToolsModMobEffects.FROZEN.get()).getDuration() : 0;
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(BetterToolsModMobEffects.FROZEN.get());
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(BetterToolsModMobEffects.FROZEN.get(), (int) (time - 10), 0, true, false));
 		}
 	}
 }
