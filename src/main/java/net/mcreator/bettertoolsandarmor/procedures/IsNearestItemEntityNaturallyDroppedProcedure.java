e23c48e44923c214807049e3b36dd31a2f61ab93
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-10-24 15:10:56 +0100

    Fixed a null-pointer exception with Smelting Touch, when no items were detected within a certain range.

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/IsNearestItemEntityNaturallyDroppedProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/IsNearestItemEntityNaturallyDroppedProcedure.java
@@ -11,36 +11,21 @@ import java.util.Comparator;
 
 public class IsNearestItemEntityNaturallyDroppedProcedure {
 	public static boolean execute(LevelAccessor world, double x, double y, double z) {
-		double dupe_chance = 0;
-		return new Object() {
-			public double getValue() {
-				CompoundTag dataIndex = new CompoundTag();
-				((Entity) world.getEntitiesOfClass(ItemEntity.class, AABB.ofSize(new Vec3((x + 0.5), (y + 0.8), (z + 0.5)), 1.25, 1.25, 1.25), e -> true).stream().sorted(new Object() {
-					Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
-						return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
-					}
-				}.compareDistOf((x + 0.5), (y + 0.8), (z + 0.5))).findFirst().orElse(null)).saveWithoutId(dataIndex);
-				return dataIndex.getDouble("Age");
-			}
-		}.getValue() <= 5 && !((Entity) world.getEntitiesOfClass(ItemEntity.class, AABB.ofSize(new Vec3((x + 0.5), (y + 0.8), (z + 0.5)), 1.25, 1.25, 1.25), e -> true).stream().sorted(new Object() {
+		Entity nearest = null;
+		nearest = (Entity) world.getEntitiesOfClass(ItemEntity.class, AABB.ofSize(new Vec3((x + 0.5), (y + 0.8), (z + 0.5)), 1.25, 1.25, 1.25), e -> true).stream().sorted(new Object() {
 			Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
 				return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
 			}
-		}.compareDistOf((x + 0.5), (y + 0.8), (z + 0.5))).findFirst().orElse(null)).onGround()
-				&& (((Entity) world.getEntitiesOfClass(ItemEntity.class, AABB.ofSize(new Vec3((x + 0.5), (y + 0.8), (z + 0.5)), 1.25, 1.25, 1.25), e -> true).stream().sorted(new Object() {
-					Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
-						return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
-					}
-				}.compareDistOf((x + 0.5), (y + 0.8), (z + 0.5))).findFirst().orElse(null)).getDeltaMovement().x() > 0.001
-						|| ((Entity) world.getEntitiesOfClass(ItemEntity.class, AABB.ofSize(new Vec3((x + 0.5), (y + 0.8), (z + 0.5)), 1.25, 1.25, 1.25), e -> true).stream().sorted(new Object() {
-							Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
-								return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
-							}
-						}.compareDistOf((x + 0.5), (y + 0.8), (z + 0.5))).findFirst().orElse(null)).getDeltaMovement().y() != 0
-						|| ((Entity) world.getEntitiesOfClass(ItemEntity.class, AABB.ofSize(new Vec3((x + 0.5), (y + 0.8), (z + 0.5)), 1.25, 1.25, 1.25), e -> true).stream().sorted(new Object() {
-							Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
-								return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
-							}
-						}.compareDistOf((x + 0.5), (y + 0.8), (z + 0.5))).findFirst().orElse(null)).getDeltaMovement().z() > 0.001);
+		}.compareDistOf((x + 0.5), (y + 0.8), (z + 0.5))).findFirst().orElse(null);
+		if (nearest instanceof ItemEntity) {
+			return new Object() {
+				public double getValue() {
+					CompoundTag dataIndex = new CompoundTag();
+					nearest.saveWithoutId(dataIndex);
+					return dataIndex.getDouble("Age");
+				}
+			}.getValue() <= 5 && !nearest.onGround() && (nearest.getDeltaMovement().x() > 0.001 || nearest.getDeltaMovement().y() != 0 || nearest.getDeltaMovement().z() > 0.001);
+		}
+		return false;
 	}
 }
