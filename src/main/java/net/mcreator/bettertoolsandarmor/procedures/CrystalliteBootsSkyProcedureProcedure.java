a60f17d19c17459c389a26fff4e93453cd045e7f
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-10-29 15:51:28 +0000

    Refactored code for Sky Crystallite Boots. Winged Boots now have a similar effect, but fewer blocks.

---
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteBootsSkyProcedureProcedure.java
@@ -0,0 +1,50 @@
+package net.mcreator.bettertoolsandarmor.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
+
+import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class CrystalliteBootsSkyProcedureProcedure {
+	@SubscribeEvent
+	public static void onEntityFall(LivingFallEvent event) {
+		if (event != null && event.getEntity() != null) {
+			execute(event, event.getEntity(), event.getDistance());
+		}
+	}
+
+	public static void execute(Entity entity, double distance) {
+		execute(null, entity, distance);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity, double distance) {
+		if (entity == null)
+			return;
+		double safe_distance = 0;
+		if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == BetterToolsModItems.WINGED_BOOTS_BOOTS.get()) {
+			safe_distance = 5;
+		}
+		if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_SKY_BOOTS.get()) {
+			safe_distance = 8;
+		}
+		if (safe_distance > 0) {
+			if (distance < safe_distance) {
+				if (event != null && event.isCancelable()) {
+					event.setCanceled(true);
+				} else if (event != null && event.hasResult()) {
+					event.setResult(Event.Result.DENY);
+				}
+			}
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/LowGravityAdvProcedureProcedure.java
@@ -0,0 +1,53 @@
+package net.mcreator.bettertoolsandarmor.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.ForgeMod;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import net.mcreator.bettertoolsandarmor.init.BetterToolsModItems;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class LowGravityAdvProcedureProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player);
+		}
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		double safe_distance = 0;
+		if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == BetterToolsModItems.CRYSTALLITE_ARMOR_SKY_BOOTS.get()
+				|| (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == BetterToolsModItems.WINGED_BOOTS_BOOTS.get()) {
+			if (((LivingEntity) entity).getAttribute(ForgeMod.ENTITY_GRAVITY.get()).getValue() < ((LivingEntity) entity).getAttribute(ForgeMod.ENTITY_GRAVITY.get()).getBaseValue()) {
+				if (entity instanceof ServerPlayer _player) {
+					Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("better_tools:low_gravity_adv"));
+					AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+					if (!_ap.isDone()) {
+						for (String criteria : _ap.getRemainingCriteria())
+							_player.getAdvancements().award(_adv, criteria);
+					}
+				}
+			}
+		}
+	}
+}
